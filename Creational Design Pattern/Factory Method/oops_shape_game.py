6b7848b8989f232d6595528b10d080af3ab0699b
SakshamRatra <sakshamratra.0106@gmail.com>
2023-01-20 17:08:52 +0530

    Factory Method

---
+++ Creational Design Pattern/Factory Method/oops_shape_game.py
@@ -0,0 +1,76 @@
+import pygame
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Shape:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self, direction):
+
+        if direction == 'up':
+            self.y -= 4
+        elif direction == 'down':
+            self.y += 4
+        elif direction == 'left':
+            self.x -= 4
+        else:
+            self.x += 4
+
+    def draw(self):
+        return NotImplementedError()
+
+
+class Square(Shape):
+
+    def draw(self):
+        pygame.draw.rect(
+            screen,
+            (255, 255, 0),
+            pygame.Rect(self.x, self.y, 20, 20)
+        )
+
+
+class Circle(Shape):
+
+    def draw(self):
+        pygame.draw.circle(
+            screen,
+            (0, 255, 255),
+            (self.x, self.y),
+            100
+        )
+
+
+if __name__ == '__main__':
+    window_dimensions = 800, 600
+    screen = pygame.display.set_mode(window_dimensions)
+
+    square = Circle(100, 100)
+    obj = square
+
+    player_quits = False
+
+    while not player_quits:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                player_quits = True
+
+            pressed = pygame.key.get_pressed()
+
+            if pressed[pygame.K_UP]: obj.move('up')
+            if pressed[pygame.K_DOWN]: obj.move('down')
+            if pressed[pygame.K_LEFT]: obj.move('left')
+            if pressed[pygame.K_RIGHT]: obj.move('right')
+
+            screen.fill((0, 0, 0))
+
+            obj.draw()
+
+        pygame.display.flip()
